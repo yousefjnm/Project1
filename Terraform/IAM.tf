0b8fcf2935d81a6599c4070c1c7945e068695a91
yousefjnm <35316048+yousefjnm@users.noreply.github.com>
2019-11-07 22:24:24 -0800

    Update IAM.tf

--- Terraform/IAM.tf
+++ Terraform/IAM.tf
@@ -1,61 +1,34 @@
-terraform {
-    backend  "s3" {
-    region         = "us-west-2"
-    bucket         = "gitlegionbucket"
-    key            = "ec2/terraform.tfstate"
-    dynamodb_table = "tf-state-lock"
-    }
-}
-
-resource "aws_iam_role" "lambdaRole" {
-  name = "lambdaRole"
-
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-          "Service": "ec2.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
-    }
-EOF
-}
-
-resource "aws_iam_policy" "policy" {
-  name        = "test-policy"
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_policy"
+  role = "${aws_iam_role.lambda_role.id}"
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Effect": "Allow",
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],
-      "Resource": "arn:aws:logs:*:*:*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:Start*",
-        "ec2:Stop*"
-      ],
-      "Resource": "*"
+			"Sid": "VisualEditor0",
+			"Effect": "Allow",
+			"Action": [
+				"ec2:DescribeInstances",
+				"ec2:StartInstances",
+				"logs:*",
+				"ec2:StopInstances"
+			],
+			"Resource": "*"
     }
   ]
 }
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "attach-policies" {
-  role       = "${aws_iam_role.lambdaRole.name}"
-  policy_arn = "${aws_iam_policy.policy.arn}"
+  #policy = "${file("lambda-policy.json")}"
+
+  
+
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda4_role"
+
+  assume_role_policy = "${file("lambda-assume-policy.json")}"
+  
 }
