75511eba0f02929107896c668a22f00881fdc661
yousefjnm <35316048+yousefjnm@users.noreply.github.com>
2019-11-14 01:29:10 -0800

    Update ALB.tf

--- Terraform/ALB.tf
+++ Terraform/ALB.tf
@@ -8,7 +8,8 @@ resource "aws_alb" "webapp_load_balancer" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["${aws_security_group.elb_security_group.id}"]
-  subnets            = ["subnet-0272634f5cdc4c886", "subnet-04f078b91b5bd1841"]
+  subnets            = ["${aws_subnet.public_1.id}", "${aws_subnet.public_2.id}", "${aws_subnet.public_3.id}"]
+  #subnets            = ["subnet-0272634f5cdc4c886", "subnet-04f078b91b5bd1841"]
   #depends_on         = ["aws_subnet.public_subnets",   "aws_security_group.elb_security_group"]
 
   enable_deletion_protection = true
@@ -26,7 +27,7 @@ resource "aws_alb" "webapp_load_balancer" {
 resource "aws_security_group" "elb_security_group" {
   name = "ELB-SG"
   description = "ELB Security Group"
-  #vpc_id = "${aws_vpc.mainvpc.id}"  ########this is the vpc
+  vpc_id = "${aws_vpc.VPC_Official.id}"  ########this is the vpc
  
   ingress {
     from_port = 80
@@ -54,7 +55,7 @@ resource "aws_security_group" "elb_security_group" {
 ##create target group
 resource "aws_alb_target_group" "alb_front_http" {
   name = "alb-front-http"
-  vpc_id = "vpc-d1098ba9"     #####This is our vpc
+  vpc_id = "${aws_vpc.VPC_Official.id}"     #####This is our vpc
   port = "80"
   protocol  = "HTTP"
   health_check {
@@ -70,7 +71,7 @@ resource "aws_alb_target_group" "alb_front_http" {
   tags = {
     Name = "alb_front_http_Tf"
   }
-  #depend_on = ["aws_vpc.mainvpc"]]   ###this is our vpc
+  #depends_on = ["${aws_vpc.VPC_Official.id}"]   ###this is our vpc
 }
 
 
@@ -80,7 +81,8 @@ resource "aws_alb_target_group_attachment" "targetgroup_alb" {
   target_group_arn = "${aws_alb_target_group.alb_front_http.arn}"
   #count = "${length(var.public_subnet_cidr)}"
   port = 80
-  target_id = "i-007d4f45a6587495e"
+  target_id = "${aws_instance.WebSrvr.id}"   ###This is the EC2 
+  #target_id = "i-007d4f45a6587495e"   ###This is the EC2 ${aws_instance.test.id}
 }
 
 
@@ -119,3 +121,4 @@ resource "aws_lb_listener" "front_end2" {
   }
   depends_on = ["aws_alb.webapp_load_balancer", "aws_alb_target_group.alb_front_http"]
 }
+
