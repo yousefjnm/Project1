16024bd0bbb5fb3538ee7f164cf40c2ff4013730
Laura Hamilton <laura.hamilton.790@my.csun.edu>
2019-11-14 19:49:13 -0800

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -77,7 +77,19 @@ resource "aws_security_group" "SG_Official" {
   name         = "SG_Official"
   description  = "default VPC security group"
   ingress {
+    from_port    = 22
+    to_port      = 22
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"] 
+}
+  ingress {
     from_port    = 80
+    to_port      = 80
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"] 
+}
+   ingress {
+    from_port    = 443
     to_port      = 443
     protocol     = "tcp"
     cidr_blocks  = ["0.0.0.0/0"] 
